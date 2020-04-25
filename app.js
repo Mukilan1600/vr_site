0dd30c57f1f5eb89acd88e04e0b54e1f7dae4d15
Mukilan1600 <ak-muki@hotmail.com>
2020-04-25 14:19:05 +0530

    first

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var body_parser = require("body-parser");
+var cors = require("cors");
+var mongoose = require("mongoose");
+var morgan = require("morgan");
+
+var app = express();
+
+app.use(cors());
+app.use(morgan("default"));
+app.use(body_parser.json());
+
+const port = process.env.PORT || 8080;
+
+app.get("/",(req,res) => {
+    res.send("Hello");
+});
+
+app.listen(port,()=>{
+    console.log("Listening on port "+port);
+});
