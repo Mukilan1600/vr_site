4fd69e89ed929bb1aba10d8a9c45436652d58d18
Mukilan1600 <ak-muki@hotmail.com>
2020-04-25 15:11:28 +0530

    Added front end

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ var body_parser = require("body-parser");
 var cors = require("cors");
 var mongoose = require("mongoose");
 var morgan = require("morgan");
+var path = require("path");
 
 var app = express();
 
+//Require cors and morgan for cross-site and debugging
 app.use(cors());
 app.use(morgan("default"));
 app.use(body_parser.json());
 
-const port = process.env.PORT || 8080;
+//Set static path
+app.use(express.static(path.join(__dirname,'public')));
+
+const port = process.env.PORT || 3000;
 
 app.get("/",(req,res) => {
     res.send("Hello Lord explosion diarrhea");
